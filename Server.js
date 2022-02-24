9624bb8c1ea07c0f7ad7647949c7d9c61881f290
Brando Collins <ghost.rkrv3@gmail.com>
2022-02-24 16:33:48 -0600

    admin updates

--- Server.js
+++ Server.js
@@ -1,4 +1,4 @@
-const fs = require("fs")
+const fs = require("fs");
 
 const express = require("express");
 
@@ -7,8 +7,11 @@ const app = express();
 
 app.post("/join", async (req, res, next) => {
 try {
-let contact = req.body
-fs.appendFile("./BPNMembersSpreadsheet.csv", `${new Date().toString()},${contact.firstName},${contact.lastName},${contact.email},${contact.city},${contact.state},${contact.phoneNumber},${contact.preferredContactMethod},${contact.areYoucheckAllthatApply},$contact.pleaseTellusMoreaboutYourselfwhatWouldyouLiketheGrouptoKnowaboutYouieEmployercareerGoalsentrepreneurFieldofKnowledgeexperience},${contact.DoyouGivepermissionForyourInformationtoBeIncludedinTheBPNdirectory},${contact.howDidyouHearaboutBPN},${contact.additionalCommentsquestionsSuggestions}`)
+let contact = req.body;
+fs.appendFile("./BPNMembersSpreadsheet.csv", `${new Date().toString()},${contact.firstName},${contact.lastName},${contact.email},${contact.city},${contact.state},${contact.phoneNumber},${contact.preferredContactMethod},${contact.areYoucheckAllthatApply},$contact.pleaseTellusMoreaboutYourselfwhatWouldyouLiketheGrouptoKnowaboutYouieEmployercareerGoalsentrepreneurFieldofKnowledgeexperience},${contact.DoyouGivepermissionForyourInformationtoBeIncludedinTheBPNdirectory},${contact.howDidyouHearaboutBPN},${contact.additionalCommentsquestionsSuggestions}`, () => {
+res.json({ message: "Thank you for joining BPN" })
+
+})
 
 }
 
@@ -24,3 +27,5 @@ next(e)
 
 
 })
+
+app.listen(port)
